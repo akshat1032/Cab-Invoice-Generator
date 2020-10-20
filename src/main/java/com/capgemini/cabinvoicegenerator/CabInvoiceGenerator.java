4a705400513cf68600c9e56367018c67c70714b1
akshat1032 <akshat.30oct@gmail.com>
2020-10-20 15:31:01 +0530

    Added : Welcome message added

---
+++ src/main/java/com/capgemini/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -0,0 +1,13 @@
+package com.capgemini.cabinvoicegenerator;
+
+import java.util.logging.Logger;
+
+public class CabInvoiceGenerator {
+
+	public static void main(String[] args) {
+		Logger log = Logger.getLogger(CabInvoiceGenerator.class.getName());
+
+		// Welcome message added
+		log.info("Welcome to Cab Invoice Generator!");
+	}
+}
