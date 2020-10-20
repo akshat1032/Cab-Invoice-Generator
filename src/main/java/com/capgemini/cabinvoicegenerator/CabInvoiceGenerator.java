670ae09a52106cbcbd8118e04d292567b889366f
akshat1032 <akshat.30oct@gmail.com>
2020-10-20 17:57:53 +0530

    Refactor : Return invoice summary based on user id and ride type tested and refactored

--- src/main/java/com/capgemini/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/capgemini/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -2,32 +2,46 @@ package com.capgemini.cabinvoicegenerator;
 
 public class CabInvoiceGenerator {
 
+	public enum RideCategory {
+		NORMAL_RIDES, PREMIUM_RIDES;
+	}
+
 	// Constant value for cost
 	private static final double COST_PER_KM = 10;
 	private static final double COST_PER_MINUTE = 1;
 	private static final double MINIMUM_FARE = 5;
+	private static final double COST_PER_KM_PREMIUM = 15;
+	private static final double COST_PER_MINUTE_PREMIUM = 2;
+	private static final double MINIMUM_FARE_PREMIUM = 20;
+
 	private final RideRepository USER_RIDE_DETAIL;
 
 	public CabInvoiceGenerator() {
-		this.USER_RIDE_DETAIL = null;}
-	
+		this.USER_RIDE_DETAIL = null;
+	}
+
 	// Initializing the user ride detail field
 	public CabInvoiceGenerator(String userId, Rides[] rides) {
 		USER_RIDE_DETAIL = new RideRepository(userId, rides);
 	}
 
 	// Returning total fare
-	public double calculateTotalFare(double distance, double time) {
-		double totalFare = distance * COST_PER_KM + time * COST_PER_MINUTE;
-		return Math.max(totalFare, MINIMUM_FARE);
+	public double calculateTotalFare(double distance, double time, RideCategory rideCategory) {
+		double totalFare = 0.0;
+		if (rideCategory.equals(RideCategory.NORMAL_RIDES)) {
+			totalFare = distance * COST_PER_KM + time * COST_PER_MINUTE;
+			return Math.max(totalFare, MINIMUM_FARE);
+		}
+		totalFare = distance * COST_PER_KM_PREMIUM + time * COST_PER_MINUTE_PREMIUM;
+		return Math.max(totalFare, MINIMUM_FARE_PREMIUM);
 	}
 
 	// Returning enhanced invoice
-	public InvoiceSummary calculateTotalFare(String userId) {
-		Rides [] rides = USER_RIDE_DETAIL.getRides(userId);
+	public InvoiceSummary calculateTotalFare(String userId, RideCategory rideCategory) {
+		Rides[] rides = USER_RIDE_DETAIL.getRides(userId);
 		double aggregateFare = 0.0;
 		for (Rides ride : rides) {
-			aggregateFare += this.calculateTotalFare(ride.getDistance(), ride.getTime());
+			aggregateFare += this.calculateTotalFare(ride.getDistance(), ride.getTime(), rideCategory);
 		}
 		return new InvoiceSummary(rides.length, aggregateFare);
 	}
--- src/test/java/com/capgemini/cabinvoicegeneratortester/TestCabInvoiceGenerator.java
+++ src/test/java/com/capgemini/cabinvoicegeneratortester/TestCabInvoiceGenerator.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.capgemini.cabinvoicegenerator.CabInvoiceGenerator;
+import com.capgemini.cabinvoicegenerator.CabInvoiceGenerator.RideCategory;
 import com.capgemini.cabinvoicegenerator.InvoiceSummary;
 import com.capgemini.cabinvoicegenerator.Rides;
 
@@ -15,17 +16,17 @@ public class TestCabInvoiceGenerator {
 
 	public static Logger log = Logger.getLogger(CabInvoiceGenerator.class.getName());
 	CabInvoiceGenerator cabInvoiceGenerator = null;
-	
+
 	@Before
-	public void setUp() throws Exception{
+	public void setUp() throws Exception {
 		cabInvoiceGenerator = new CabInvoiceGenerator();
 	}
-	
+
 	// Test for total fare given distance and time
 	@Test
 	public void givenDistanceAndTimeReturnTotalFare() {
 		double distance = 2.0, time = 20.0;
-		Assert.assertEquals(40, cabInvoiceGenerator.calculateTotalFare(distance, time), 0.0);
+		Assert.assertEquals(40, cabInvoiceGenerator.calculateTotalFare(distance, time, RideCategory.NORMAL_RIDES), 0.0);
 		log.info("Total fare returned tested succesfully");
 	}
 
@@ -33,7 +34,7 @@ public class TestCabInvoiceGenerator {
 	@Test
 	public void givenDateAndTimeReturnMinimumFare() {
 		double distance = 0.2, time = 2;
-		Assert.assertEquals(5, cabInvoiceGenerator.calculateTotalFare(distance, time), 0.0);
+		Assert.assertEquals(5, cabInvoiceGenerator.calculateTotalFare(distance, time, RideCategory.NORMAL_RIDES), 0.0);
 		log.info("Return minimum fare tested successfully");
 	}
 
@@ -44,7 +45,38 @@ public class TestCabInvoiceGenerator {
 		Rides[] rides = { new Rides(2.0, 20.0), new Rides(0.2, 2.0) };
 		CabInvoiceGenerator cabInvoiceGeneratorParam = new CabInvoiceGenerator(userId, rides);
 		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 45);
-		InvoiceSummary actualInvoiceSummary = cabInvoiceGeneratorParam.calculateTotalFare(userId);
+		InvoiceSummary actualInvoiceSummary = cabInvoiceGeneratorParam.calculateTotalFare(userId,
+				RideCategory.NORMAL_RIDES);
+		Assert.assertEquals(expectedInvoiceSummary, actualInvoiceSummary);
+		log.info("Return invoice summary for userId tested successfully");
+	}
+
+	// Test for total fare given distance and time
+	@Test
+	public void givenDistanceAndTimeReturnTotalFarePremium() {
+		double distance = 2.0, time = 20.0;
+		Assert.assertEquals(70, cabInvoiceGenerator.calculateTotalFare(distance, time, RideCategory.PREMIUM_RIDES),
+				0.0);
+		log.info("Total fare returned tested succesfully");
+	}
+
+	// Test for minimum fare given distance and time
+	@Test
+	public void givenDateAndTimeReturnMinimumFarePremium() {
+		double distance = 0.2, time = 2;
+		Assert.assertEquals(20, cabInvoiceGenerator.calculateTotalFare(distance, time, RideCategory.PREMIUM_RIDES),0.0);
+		log.info("Return minimum fare tested successfully");
+	}
+
+	// Test for invoice summary for multiple rides based on userId
+	@Test
+	public void givenMultipleRidesReturnInvoiceSummaryPremium() {
+		String userId = "Shino";
+		Rides[] rides = { new Rides(2.0, 20.0), new Rides(0.2, 2.0) };
+		CabInvoiceGenerator cabInvoiceGeneratorParam = new CabInvoiceGenerator(userId, rides);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 90);
+		InvoiceSummary actualInvoiceSummary = cabInvoiceGeneratorParam.calculateTotalFare(userId,
+				RideCategory.PREMIUM_RIDES);
 		Assert.assertEquals(expectedInvoiceSummary, actualInvoiceSummary);
 		log.info("Return invoice summary for userId tested successfully");
 	}
